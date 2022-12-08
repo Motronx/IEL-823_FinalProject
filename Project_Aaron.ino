651452485884a7bb74789c6fc099aa622232ab6e
Motronx <aaron.1605le@gmail.com>
2022-12-08 00:46:02 -0600

    Se adjunta una pruba para el nivel de hardware mas el test

--- Project_Aaron.ino
+++ Project_Aaron.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>            //Se llama a la libreria de arduino para que reconoceza las funciones porpias de el
+#include "testinghall.h"        //Se llama a la libreria .h de nuestro proyecto
+#include "angle.h"              //Se llama a la libreria de encabezado
 
+static angle_t angle;
+int angle_val;
+int magnetics_val;
+
+void application_init ( void )
+{
+    angle_cfg_t cfg;
+    angle_cfg_setup( &cfg );
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void application_task ( void )
+{
+    angle_val = angle_get_angle( &angle );
+    magnetics_val = angle_get_magnetics( &angle );    
+    delay(1000);
 }
