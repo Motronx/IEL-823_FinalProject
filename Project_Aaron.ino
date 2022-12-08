02e0ad61913ccc789217749769b93df521aee702
Motronx <aaron.1605le@gmail.com>
2022-12-08 01:17:08 -0600

    Se adjunta las pruebas verificadas para el sensor

--- Project_Aaron.ino
+++ Project_Aaron.ino
@@ -6,6 +6,55 @@ static angle_t angle;
 int angle_val;
 int magnetics_val;
 
+float distancia;
+float voltaje;
+float vmin = 3.5;
+float resultado = -0.08 * distancia + 4;
+float temp;
+float tempideal = 25;
+
+String mensaje1 = "Ingrese Posicionamiento en grados";
+String mensaje2 = "Sensor NO se detecta";
+String mensaje3 = "Sensor SI se detecta";
+String mensaje5 = "Ingrese Temperatura";
+String mensaje6 = "Su distancia y temperatura son: ";
+
+void setup(){
+	Serial.begin(9600);
+}
+
+void loop(){
+	Serial.println("*****************");
+  	Serial.println(mensaje1);
+  while (Serial.available()==0){
+  }
+  	distancia = Serial.read();
+  	distancia = Serial.parseInt();
+  	Serial.println(mensaje5);
+  while (Serial.available()==0){
+  }
+  	temp = Serial.read();
+  	temp = Serial.parseInt();
+  resultado = -0.08 * distancia + 4;
+  if(resultado < vmin & (temp == tempideal)){
+    Serial.println();
+    Serial.println(mensaje2);
+    //Serial.println(temp);
+  }
+  else{
+    Serial.println();
+    Serial.println(mensaje3);
+    Serial.println();
+    }
+  
+  distancia = 0;
+  distancia = Serial.parseInt();
+   temp = 0;
+  temp = Serial.parseInt();
+
+}
+
+
 void application_init ( void )
 {
     angle_cfg_t cfg;
